a1346d451c8c33617a2e914d714a07b820c340ea
Marvin Jason Sy <marvinjason.sy@gmail.com>
2017-06-12 23:09:22 +0800

    Added ShortestJobFirst

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,21 @@
 
 import java.util.List;
 
-
 public class Main
 {
     public static void main(String[] args)
     {
-        fcfs();
+        // Done: fcfs();
+        // Done: sjf();
     }
     
     public static void fcfs()
     {
         CPUScheduler fcfs = new FirstComeFirstServe();
-        
         fcfs.add(new Row("P1", 0, 5));
         fcfs.add(new Row("P2", 2, 4));
         fcfs.add(new Row("P3", 4, 3));
         fcfs.add(new Row("P4", 6, 6));
-        
         fcfs.process();
         
         System.out.println("Process\tAT\tBT\tWT\tTAT");
@@ -45,6 +43,34 @@ public class Main
     
     public static void sjf()
     {
+        CPUScheduler sjf = new ShortestJobFirst();
+        sjf.add(new Row("P1", 0, 5));
+        sjf.add(new Row("P2", 2, 3));
+        sjf.add(new Row("P3", 4, 2));
+        sjf.add(new Row("P4", 6, 4));
+        sjf.add(new Row("P5", 7, 1));
+        sjf.process();
         
+        System.out.println("Process\tAT\tBT\tWT\tTAT");
+
+        for (Row row : sjf.getRows())
+        {
+            System.out.println(row.getProcessName() + "\t" + row.getArrivalTime() + "\t" + row.getBurstTime() + "\t" + row.getWaitingTime() + "\t" + row.getTurnaroundTime());
+        }
+        
+        System.out.println();
+        
+        for (int i = 0; i < sjf.getTimeline().size(); i++)
+        {
+            List<Event> timeline = sjf.getTimeline();
+            System.out.print(timeline.get(i).getStartTime() + "(" + timeline.get(i).getProcessName() + ")");
+            
+            if (i == sjf.getTimeline().size() - 1)
+            {
+                System.out.print(timeline.get(i).getFinishTime());
+            }
+        }
+        
+        System.out.println();
     }
 }
--- src/ShortestJobFirst.java
+++ src/ShortestJobFirst.java
@@ -1,9 +1,88 @@
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ShortestJobFirst extends CPUScheduler
 {
     @Override
     public void process()
     {
+        Collections.sort(this.getRows(), (Object o1, Object o2) -> {
+            if (((Row) o1).getArrivalTime() == ((Row) o2).getArrivalTime())
+            {
+                return 0;
+            }
+            else if (((Row) o1).getArrivalTime() < ((Row) o2).getArrivalTime())
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        });
         
+        List<Row> rows = deepCopy(this.getRows());
+        int time = rows.get(0).getArrivalTime();
+        
+        while (!rows.isEmpty())
+        {
+            List<Row> availableRows = new ArrayList();
+            
+            for (Row row : rows)
+            {
+                if (row.getArrivalTime() <= time)
+                {
+                    availableRows.add(row);
+                }
+            }
+            
+            Collections.sort(availableRows, (Object o1, Object o2) -> {
+                if (((Row) o1).getBurstTime() == ((Row) o2).getBurstTime())
+                {
+                    return 0;
+                }
+                else if (((Row) o1).getBurstTime() < ((Row) o2).getBurstTime())
+                {
+                    return -1;
+                }
+                else
+                {
+                    return 1;
+                }
+            });
+            
+            Row row = availableRows.get(0);
+            this.getTimeline().add(new Event(row.getProcessName(), time, time + row.getBurstTime()));
+            time += row.getBurstTime();
+            
+            for (int i = 0; i < rows.size(); i++)
+            {
+                if (rows.get(i).getProcessName().equals(row.getProcessName()))
+                {
+                    rows.remove(i);
+                    break;
+                }
+            }
+        }
+        
+        for (Row row : this.getRows())
+        {
+            row.setWaitingTime(this.getEvent(row).getStartTime() - row.getArrivalTime());
+            row.setTurnaroundTime(row.getWaitingTime() + row.getBurstTime());
+        }
+    }
+    
+    private List<Row> deepCopy(List<Row> oldList)
+    {
+        List<Row> newList = new ArrayList();
+        
+        for (Row row : oldList)
+        {
+            newList.add(row);
+        }
+        
+        return newList;
     }
 }
